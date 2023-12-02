9269a2b2dad97762c9dc36014b42353f5537895c
Alejandro Martinez <alejandroa@kannactnow.com>
2023-12-02 20:57:52 +0100

    Helper to build project strucutre

---
+++ do_day.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+YEAR=$1
+DAY=$2 
+
+echo "Doing year $YEAR day $DAY folder structure"
+for i in 1 2
+do
+    BASE_PATH="aocs/$YEAR"
+    mkdir -p $BASE_PATH/day_$DAY/$i/
+    touch $BASE_PATH/day_$DAY/$i/day_$DAY.py
+    touch $BASE_PATH/day_$DAY/$i/test_day_$DAY.py
+done
